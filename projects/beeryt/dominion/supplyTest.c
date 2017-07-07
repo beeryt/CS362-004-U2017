277bff52ecb2b3c81c242c2d2da2fdea5552902b
T. Carl Beery <beeryt@oregonstate.edu>
2017-07-06 18:20:21 -0700

    Reformatted code for readability
    Used clang-format to match LLVM style with tabs instead of spaces. Also converted everything to Allman style brackets.

--- projects/beeryt/dominion/supplyTest.c
+++ projects/beeryt/dominion/supplyTest.c
@@ -1,30 +1,31 @@
 #include "dominion.h"
 #include "dominion_helpers.h"
-#include <string.h>
-#include <stdio.h>
-#include <assert.h>
 #include "rngs.h"
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
 
 #define DEBUG 0
 #define NOISY_TEST 1
 
-int main () {
+int main()
+{
 
-  int r;
+	int r;
 
-  int k[10] = {adventurer, council_room, feast, gardens, mine,
-	       remodel, smithy, village, baron, great_hall};
+	int k[10] = {adventurer, council_room, feast,   gardens, mine,
+				 remodel,	smithy,	   village, baron,   great_hall};
 
-  struct gameState G;
-  
-  r = initializeGame(4, k, 1, &G);
+	struct gameState G;
 
-  printf ("initializeGame(4, k, 1, &G) = %d\n", r);
-  assert(r == 0);
+	r = initializeGame(4, k, 1, &G);
 
-  r = supplyCount(adventurer, &G);
-  printf ("supplyCount(adventurer, &G) = %d\n", r);
-  assert(r == 10);
-  
-  return 0;
+	printf("initializeGame(4, k, 1, &G) = %d\n", r);
+	assert(r == 0);
+
+	r = supplyCount(adventurer, &G);
+	printf("supplyCount(adventurer, &G) = %d\n", r);
+	assert(r == 10);
+
+	return 0;
 }
